4affec2ada98295b11a89569228ed31a4d674216
Ku Ahmad Mudrikah <kamudrikah@gmail.com>
2016-10-06 04:33:26 +0800

    Statistic frequency

--- Frequecy.php
+++ Frequecy.php
@@ -17,20 +17,20 @@
 					<li id="link1" class="selected">
 						<a href="index.php">HOME</a>
 					</li>
-					<li id="link4">
+<li id="link2">
 						<a href="search.php">Search</a>
 					</li>
-					
+
 					<li id="link4">
-						<a href="concordance.php"><span>Concordance</span></a>
+						<a href="concordance.php"><span>Concordance</span> </a>
 					</li>
-					<li id="link4">
-						<a href="Frequecy.php"><span>Frequecy</span></a>
-					</li>
-					<li id="link4">
+					<li id="link5">
 						<a href="past_year.php">Past Year</a>
 					</li>
-					<li id="link4">
+					<!-- <li id="link3">
+						<a href="question.php"><span>Question</span></a>
+					</li> -->
+					<li id="link6">
 						<a href="about.html">About</a>
 					</li>
 				</ul>
@@ -84,11 +84,11 @@
 				<div class="article">
 					<h2>What is FREQUENCY?</h2>
 					<p>
-						Word lists by frequency are lists of a language's words grouped by frequency of occurrence within some given text corpus, either by levels or as a ranked list, serving the purpose of vocabulary acquisition.............. 
+						Word lists by frequency are lists of a language's words grouped by frequency of occurrence within some given text corpus, either by levels or as a ranked list, serving the purpose of vocabulary acquisition..............
 					</p>
 					<h4>Search Frequency</h4>
 						<p>
-						<form name="" action="concordance.php" method="POST">
+						<form name="" action="Frequecy.php" method="POST">
 						<input type="text" name="find" value="" id="find" >
 						<input type= "submit" name="submit" value="Find"  />
 						</form>
@@ -97,116 +97,67 @@
 						<?php
 						$query= @$_POST['find'];
 						if (!empty($query)) {
-						
-						require("doc2txt.class.php");
-						
-							$docObj = new Doc2Txt("test2004.docx");
-							$docObj2 = new Doc2txt("test2002.docx");
-							$docObj3 = new Doc2Txt("test2003.docx");
-							$docObj4 = new Doc2Txt("test2005.docx");
-							$docObj5 = new Doc2Txt("test2006.docx");
-							$docObj6 = new Doc2Txt("test2007.docx");
-							$docObj7 = new Doc2Txt("test2010.docx");
-							
-							
-							
-							//$docObj = new Doc2Txt("test.doc");
 
-							$txt[2] = $docObj->convertToText();				
-							$txt[0]= $docObj2->convertToText();
-							$txt[1]= $docObj3->convertToText();
-							$txt[3]= $docObj4->convertToText();
-							$txt[4]= $docObj5->convertToText();
-							$txt[5]= $docObj6->convertToText();
-							$txt[6]= $docObj7->convertToText();
-							
-							$year[0]="2002";
-							$year[1]= "2003";
-							$year[2]= "2004";
-							$year[3]= "2005";
-							$year[4]= "2006";
-							$year[5]= "2007";
-							$year[6]= "2010";
-						
-							
-							?>
-							
+						require("doc2txt.class.php");
+
+						$docObj = new Doc2Txt("test2004.docx");
+						$docObj2 = new Doc2txt("test2002.docx");
+						$docObj3 = new Doc2Txt("test2003.docx");
+						$docObj4 = new Doc2Txt("test2005.docx");
+						$docObj5 = new Doc2Txt("test2006.docx");
+						$docObj6 = new Doc2Txt("test2007.docx");
+						$docObj7 = new Doc2Txt("test2010.docx");
+
+						$txt[0]= $docObj2->convertToText();
+						$txt[1]= $docObj3->convertToText();
+						$txt[2] = $docObj->convertToText();
+						$txt[3]= $docObj4->convertToText();
+						$txt[4]= $docObj5->convertToText();
+						$txt[5]= $docObj6->convertToText();
+						$txt[6]= $docObj7->convertToText();
+
+						$year[0]="2002";
+						$year[1]= "2003";
+						$year[2]= "2004";
+						$year[3]= "2005";
+						$year[4]= "2006";
+						$year[5]= "2007";
+						$year[6]= "2010";
+						?>
 						<table width="500" border="1" align="top">
 						<tr>
-						<td width="200" align="center">Left Context</td>
-						
+						<td width="200" align="center">Frequency</td>
+
 						<td width="70" align="center">Word</td>
-						
-						<td width="200" align="center">Right Context</td>
-						
+
 						<td width="50" align="center">Year</td>
-						
+
 						<?php
-						for ($i=1; $i<2; $i++) 
-						{	
-							//echo $txt[$i];
-							//echo substr_count($txt[$i],$query);
-							$expl = explode($query, $txt[$i]);
-							
-							if (array_key_exists('1',$expl )) {
+						$stat = array();
+						for ($i=0; $i<7; $i++){
+							preg_match_all("/.{0,20}\b$query\b/", $txt[$i], $out_left);
+							preg_match_all("/\b$query\b.{0,20}/", $txt[$i], $out_right);
+							$berapaKali = preg_match_all("/\b$query\b/", $txt[$i], $out_mid);
+							$stat[] = $berapaKali;
 
-								echo count($expl);
+									?>
 
-							// items on the left side of middle string
-							$expl_left = explode(" ", $expl[0]);
+									<tr>
+								 <td width="" align="center"><p><?php echo $berapaKali; ?></p></td>
 
-							$left_cnt = count($expl_left);
+								 <td width="" align="center"><p><?php echo $query; ?></p></td>
 
-							$new_left = $expl_left[$left_cnt-6]. " " .$expl_left[$left_cnt-5]. " " .$expl_left[$left_cnt-4]. " " .$expl_left[$left_cnt-3] . " " . $expl_left[$left_cnt-2];
+								 <td width="" align="center"><p><?php echo $year[$i];?></p></td>
+							 </tr>
 
-							// items on the right side of middle string
-							$expl_right = explode(" ", $expl[1]);
-
-							$new_right = $expl_right[1] . " " . $expl_right[2]. " " . $expl_right[3]. " " . $expl_right[4]. " " . $expl_right[5];
-
-							// new string formated
-							$new = "... " . $new_left . " " . $query . " " . $new_right . " ...";
-							$leftt = "... " . $new_left;
-							$rightt = $new_right . " ..."; }
-							
-							//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-							
-						?>	
-						<?php
-						{
-							
-						//print $new; 
-						}
-				
-						?>
-						
-						
-						
-						<?php
-						 if(!empty($query) && array_key_exists('1',$expl )) { 
-						 ?>
-						 
-						 <tr>
-						<td width="" align="center"><input id="word" name="word" type="text" value='<?php echo $leftt; ?>' size="30"/>  </td>
-						
-						<td width="" align="center"><input id="meaning" name="meaning" type="text" value='<?php echo $query; ?>' size="10"/> </td>
-						
-						<td width="" align="center"><input id="word" name="word" type="text" value='<?php echo $rightt; ?>' size="30"/>  </td>
-						
-						<td width="" align="center"><input id="meaning" name="meaning" type="text" value='<?php echo $year[$i]; ?>' size="5"/> </td>
-						
-						
-						</tr>
-						
-							 <?php }
-							 }
+							 <?php
 							}
+						}
 							?>
-						
+							<tr>
+								<td colspan="3">Total: <?echo array_sum($stat);?></td>
+							</tr>
 						</table>
-						
-						
-					
 				</div>
 			</div>
 		</div>
