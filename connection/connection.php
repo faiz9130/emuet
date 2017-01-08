6587e2c716b1e8a2f72ff137535c70dac447e3c0
Ku Ahmad Mudrikah <kamudrikah@gmail.com>
2017-01-08 15:32:44 +0800

    connection

--- connection/connection.php
+++ connection/connection.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "emuet";
+$username = "duriantu_emuet";
+$password = "emuet";
+$dbname = "duriantu_emuet";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
